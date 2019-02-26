f73e336a2abf6a0f2ebab0f21937f790bd92f3c2
Szymon Frankowski <sz.frankyy@gmail.com>
2019-02-26 23:19:41 +0100

    class transition and tests

--- src/classes/Transition.ts
+++ src/classes/Transition.ts
@@ -1,9 +1,46 @@
 import { MMatrix } from './MMatrix';
 import { Select } from './Select';
+import  SSet from './SSet';
 
-export  class Transition<T> {
-    constructor(mmatrix:MMatrix<T>) {
+export class Transition<T> {
+    constructor(mmatrix:MMatrix<T>,uppermmatrix:MMatrix<T>) {
         this.mmatrix =  mmatrix;
+        this.uppermatrix = uppermmatrix;
     }
+
     private mmatrix: MMatrix<T>;
+    private uppermatrix:MMatrix<T>;
+
+    public getMmatrix ():MMatrix<T>{
+
+        return this.mmatrix;
+    }
+
+    public setMmatrix(mmatrix:MMatrix<T>) {
+        this.mmatrix = mmatrix;
+    }
+
+
+    public getUpperMmatrix ():MMatrix<T>{
+
+        return this.uppermatrix;
+    }
+
+    public setUpperMmatrix(uppermmatrix:MMatrix<T>) {
+        this.uppermatrix = uppermmatrix;
+    }
+
+    public markSelected() {
+
+    }
+
+    public run(x:number,y:number):void{
+
+    }
+
+    public runSpecial(x:number,y:number,sset:Set<SSet<[number, number]>>):void{
+        
+    }
+
+
 }
--- tests/transition.spec.ts
+++ tests/transition.spec.ts
@@ -1,12 +1,44 @@
-import { MMatrix } from '../src/classes/MMatrix';
-import { Transition } from '../src/classes/Transition';
-let mm = new MMatrix<[number,number]>(5,5);
-let transition = new Transition<[number,number]>(mm);
-describe('transition',()=>{
+import { MMatrix } from "../src/classes/MMatrix";
+import { Transition } from "../src/classes/Transition";
+import deepEqual from "../src/helpers/deepEqual";
 
-    test('Removing tiles',()=>{
-        
-        expect(true).toBeTruthy();
-    });
+let mm = new MMatrix<number>(5, 5);
+let upmm = new MMatrix<number>(5, 5);
+let trans = new Transition<number>(mm, upmm);
 
+mm.consumeColumns(
+  [0, 1, 2, 0, 0],
+  [0, 1, 2, 1, 0],
+  [0, 1, 3, 1, 0],
+  [0, 1, 1, 1, 0],
+  [0, 1, 2, 0, 0]
+);
+
+upmm.consumeColumns(
+  [2, 1, 2, 0, 0],
+  [3, 1, 2, 1, 0],
+  [0, 1, 2, 1, 0],
+  [0, 1, 1, 1, 0],
+  [1, 2, 2, 1, 1]
+);
+
+let transition = new Transition<number>(mm, upmm);
+describe("transition", () => {
+  test("Removing tiles", () => {
+    trans.run(1, 2);
+    expect(
+      deepEqual(mm.getArray(), [
+        [
+          [0, 1, 3, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 1, 2, 1, 0],
+          [0, 1, 1, 1, 0],
+          [0, 1, 2, 0, 0]
+        ]
+      ])
+    ).toBeTruthy();
+
+
+  });
 });
+
